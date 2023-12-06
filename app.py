b5f1fe9fac4de4320f83f728314948f2b4a78a71
YoonGeonSON <151480776+YoonGeonSON@users.noreply.github.com>
2023-12-06 16:44:07 +0900

    modified:   app.py 	modified:   resources/recipe.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.recipe import RecipeListResource, RecipeResource
+from resources.recipe import RecipeListResource, RecipeResource,RecipePublishResource
 
 app = Flask(__name__)
 
@@ -14,7 +14,8 @@ api = Api(app)
 
 # 경로(path)와 리소스(API코드)를 연결한다
 api.add_resource( RecipeListResource ,'/recipes')
-api.add_resource( RecipeResource ,'/recipes/<int:recipe_id>')
+api.add_resource( RecipeResource ,'/recipes/<int:recipe_id/>')
+api.add_resource( RecipePublishResource,'/recipes/<int:recipe_id>/publish')
 
 
 if __name__ == '__main__':
